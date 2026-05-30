bc19e64c4b198a1d002206a025ea2c1cdbf58eba
dikotl <188217911+dikotl@users.noreply.github.com>
2026-05-30 15:43:32 +0300

    Rewrite fibonacci test

--- Edl.Tests/ExecutionTests.cs
+++ Edl.Tests/ExecutionTests.cs
@@ -1,64 +1,10 @@
 using Edl.Core.Interpreter;
-using Edl.Core.Interpreter.Intrinsics.Arithmetics;
-using Edl.Core.Interpreter.Intrinsics.Comparison;
 
 namespace Edl.Tests;
 
 [TestClass]
 public class ExecutionTests
 {
-    private static IEnumerable<ICommand> GetFibonacciSetupCommands()
-    {
-        var fibBody = new ICommand[]
-        {
-            new LoadCommand("n"),
-            new PushCommand(new IntValue(2)),
-            new LessThanCommand(),
-            new IfCommand(
-                trueBranch:
-                [
-                    new LoadCommand("n")
-                ],
-                falseBranch:
-                [
-                    new LoadCommand("n"),
-                    new PushCommand(new IntValue(1)),
-                    new SubtractCommand(),
-                    new CallCommand("fib", 1),
-
-                    new LoadCommand("n"),
-                    new PushCommand(new IntValue(2)),
-                    new SubtractCommand(),
-                    new CallCommand("fib", 1),
-
-                    new AddCommand()
-                ]
-            )
-        };
-        return
-        [
-            new MakeClosureCommand(["n"], fibBody, default),
-            new StoreCommand("fib")
-        ];
-    }
-
-    [TestMethod]
-    public void Fibonacci10()
-    {
-        var context = new Core.Interpreter.Context();
-        context.Execute(GetFibonacciSetupCommands());
-
-        var testCommands = new ICommand[]
-        {
-            new PushCommand(new IntValue(10)),
-            new CallCommand("fib", 1)
-        };
-        context.Execute(testCommands);
-        var result = (IntValue)context.Pop();
-
-        Assert.AreEqual(55, result.Data);
-    }
-
     [TestMethod]
     [DataRow(0L, 0L)]
     [DataRow(1L, 1L)]
@@ -66,29 +12,45 @@ public class ExecutionTests
     [DataRow(5L, 5L)]
     [DataRow(10L, 55L)]
     [DataRow(15L, 610L)]
-    public void Fibonacci_VariousInputs_ReturnsCorrectSequence(long input, long expectedResult)
+    [DataRow(30L, 832040L)]
+    public void Fibonacci(long input, long expectedResult)
     {
-        var vm = new Core.Interpreter.Context();
-        vm.Execute(GetFibonacciSetupCommands());
-
-        var testCommands = new ICommand[]
+        var fibBody = new ICommand[]
         {
-            new PushCommand(new IntValue(input, default)),
-            new CallCommand("fib", 1)
+            new LoadCommand("n"),
+            new PushCommand(new IntValue(2)),
+            new CallCommand("<=", 2),
+            new IfCommand(
+                trueBranch: [new LoadCommand("n")],
+                falseBranch:
+                [
+                    // fib(n-2)
+                    new LoadCommand("n"),
+                    new PushCommand(new IntValue(1)),
+                    new CallCommand("-", 2),
+                    new CallCommand("fib", 1),
+                    // fib(n-2)
+                    new LoadCommand("n"),
+                    new PushCommand(new IntValue(2)),
+                    new CallCommand("-", 2),
+                    new CallCommand("fib", 1),
+                    // + fib(n-1) fib(n-2)
+                    new CallCommand("+", 2),
+                ]
+            )
         };
 
-        vm.Execute(testCommands);
-        var result = (IntValue)vm.Pop();
+        var context = new Context();
+        context.Execute([
+            // Function definition.
+            new MakeClosureCommand(["n"], fibBody, default),
+            new StoreCommand("fib"),
+            // Function call.
+            new PushCommand(new IntValue(input, default)),
+            new CallCommand("fib", 1),
+        ]);
+        var result = (IntValue)context.Pop();
 
         Assert.AreEqual(expectedResult, result.Data);
-    }
-
-    [TestMethod]
-    public void FunctionScopeCapture()
-    {
-        var vm = new Core.Interpreter.Context();
-        vm.Execute(
-        [
-        ]);
     }
 }

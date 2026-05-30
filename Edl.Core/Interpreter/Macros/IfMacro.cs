cc381997245b0f85f2f17da94cd62d474811b450
dikotl <188217911+dikotl@users.noreply.github.com>
2026-05-30 15:43:08 +0300

    `if` macro

---
+++ Edl.Core/Interpreter/Macros/IfMacro.cs
@@ -0,0 +1,67 @@
+namespace Edl.Core.Interpreter.Macros;
+
+[Macro("if")]
+public class IfMacro : IMacro
+{
+    public void Translate(Ast.Call call, Translator t)
+    {
+        ValidateArgs(call.Arguments, out var condition, out var thenExpression, out var elseExpression);
+
+        ICommand[] thenBody = InlineIfLambda(thenExpression);
+        ICommand[] elseBody = (elseExpression is not null)
+                            ? InlineIfLambda(elseExpression)
+                            : [new PushCommand(new IntValue(0))];
+
+        t.Translate(condition);
+        t.Commands.Add(new IfCommand(thenBody, elseBody));
+    }
+
+    private static ICommand[] InlineIfLambda(Ast.Expression expression)
+    {
+        if (expression is Ast.Function function)
+        {
+            return Translator.Translate(function.Body).ToArray();
+        }
+        return Translator.Translate([expression]).ToArray();
+    }
+
+    private static void ValidateArgs(
+        Ast.Expression[] args,
+        out Ast.Expression condition,
+        out Ast.Expression thenExpression,
+        out Ast.Expression? elseExpression)
+    {
+        if (args.Length != 2 && args.Length != 4)
+        {
+            throw new Exception($"Intrinsic 'if' expects 2 or 4 arguments, got {args.Length}.");
+        }
+
+        condition = args[0];
+        thenExpression = args[1];
+        elseExpression = null;
+
+        if (condition is Ast.Function)
+        {
+            throw new Exception($"Intrinsic 'if' expects the first argument not to be a function.");
+        }
+
+        if (thenExpression is Ast.Function thenBlock && thenBlock.Parameters.Length > 0)
+        {
+            throw new Exception($"Intrinsic 'if' expects then-block without parameters.");
+        }
+
+        if (args.Length == 4)
+        {
+            elseExpression = args[3];
+
+            if (args[2] is not Ast.Identifier identifier || identifier.Name != "else")
+            {
+                throw new Exception($"Intrinsic 'if' expects third argument to be identifier 'else', got {args[2]}.");
+            }
+            if (elseExpression is Ast.Function elseBlock && elseBlock.Parameters.Length > 0)
+            {
+                throw new Exception($"Intrinsic 'if' expects else-block without parameters.");
+            }
+        }
+    }
+}
